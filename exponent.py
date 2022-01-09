b76e3861dacea5b991731fc84b6fd808ab94c94d
tinsler <mapledrop@protonmail.com>
2022-01-09 13:24:02 -0600

    this is a commit

---
+++ exponent.py
@@ -0,0 +1,4 @@
+first_number = float(input("Enter a base: "))
+second_number = float(input("Enter an exponent: "))
+third_number = first_number ** second_number
+print (f"{first_number} to the power of {second_number} = {third_number}")
