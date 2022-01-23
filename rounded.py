a78c6d4b9ed7f2aa0db45b8fd8b7a1056798cb9d
tinsler <mapledrop@protonmail.com>
2022-01-23 12:01:25 -0600

    update rounded.py

--- rounded.py
+++ rounded.py
@@ -1,4 +1,3 @@
 input = float(input("Input a number: "))
-#first_number = float(input("Enter a base: "))
 rounded_input = round(input, 2)
 print(f"{input} rounded to 2 decimal places is {rounded_input}")
